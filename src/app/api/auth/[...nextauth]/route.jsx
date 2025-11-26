f4ca58217e7a6d5b3392b521854cc71be055e1b3
MaRuF2131 <marufahmmed916@gmail.com>
2025-11-26 22:21:21 +0600

    provider update

--- src/app/api/auth/[...nextauth]/route.jsx
+++ src/app/api/auth/[...nextauth]/route.jsx
@@ -1,26 +1,54 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-import EmailProvider from "next-auth/providers/email";
+import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    GoogleProvider({
-        clientId: process.env.Google_ID,
-        clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    EmailProvider({
-        server: process.env.EMAIL_SERVER,
-        from: process.env.EMAIL_FROM,
-    })
 
-    // ...add more providers here
+    // ❗ REAL EMAIL+PASSWORD LOGIN
+    CredentialsProvider({
+      name: "Credentials Login",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" }
+      },
+
+      async authorize(credentials) {
+        const { email, password } = credentials;
+        const demoUser = {
+          id: 1,
+          name: "Maruf",
+          email: "test@test.com",
+          password: "123456"
+        };
+
+        if (email === demoUser.email && password === demoUser.password) {
+          return demoUser; // SUCCESS
+        }
+
+        return null; // FAIL hole null return
+      }
+    }),
   ],
-}
+
+  pages: {
+    signIn: "/login",
+  },
+
+  session: {
+    strategy: "jwt",
+  }
+};
+
 const handler = NextAuth(authOptions);
-export  {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
