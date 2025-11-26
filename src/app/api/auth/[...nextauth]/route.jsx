820ff18bf4b1729701cf7ccbb9e941c6ca07d049
MaRuF2131 <marufahmmed916@gmail.com>
2025-11-26 20:03:53 +0600

    set http method in provider

--- src/app/api/auth/[...nextauth]/route.jsx
+++ src/app/api/auth/[...nextauth]/route.jsx
@@ -22,5 +22,5 @@ export const authOptions = {
     // ...add more providers here
   ],
 }
-
-export default NextAuth(authOptions)
+const handler = NextAuth(authOptions);
+export  {handler as GET, handler as POST};
