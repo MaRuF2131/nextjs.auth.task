0d0866a8f9d6667d9d763559c1359c440736c51e
MaRuF2131 <marufahmmed916@gmail.com>
2025-11-26 19:48:03 +0600

    provider added

---
+++ src/app/api/auth/[...nextauth]/route.jsx
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/email";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+        clientId: process.env.Google_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    EmailProvider({
+        server: process.env.EMAIL_SERVER,
+        from: process.env.EMAIL_FROM,
+    })
+
+    // ...add more providers here
+  ],
+}
+
+export default NextAuth(authOptions)
