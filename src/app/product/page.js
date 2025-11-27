2fcabb61bacc6040cab1d3a04a78a70dec20e68c
MaRuF2131 <marufahmmed916@gmail.com>
2025-11-27 12:03:54 +0600

    fetch dynamic data on product page

--- src/app/product/page.js
+++ src/app/product/page.js
@@ -1,14 +1,30 @@
+"use client"
+
+import { useEffect, useState } from "react";
 import ItemCard from "../../components/ItemCard"
-
-
-const sample = [
-{ id: 1, title: "Product A", description: "Nice product", price: 20, image: "https://picsum.photos/300" },
-{ id: 2, title: "Product B", description: "Great item", price: 15, image: "https://picsum.photos/301" },
-{ id: 3, title: "Product C", description: "Amazing", price: 30, image: "https://picsum.photos/302" },
-]
-
+import Fetch from "@/utils/fetch"
 
 export default function Items() {
+const [products, setProducts] = useState([]);
+const [loading, setLoading] = useState(true);
+
+useEffect(() => {  
+    const fetchProducts = async () => {
+        setLoading(true);
+        const res = await Fetch("/api/products","GET",null,"Failed to load products");
+        const data = res;
+        if(data.errorMsg || !data.success){
+            setProducts([]);
+        } else {
+            setProducts(data.data);
+        }
+        setLoading(false);
+    };
+    fetchProducts();
+}, []);
+if(loading){
+    return <p>Loading...</p>
+}
 return (
 <div className="p-6">
 <h2 className="text-3xl font-bold mb-2">Products</h2>
@@ -16,7 +32,7 @@ return (
 
 
 <div className="grid md:grid-cols-3 gap-6">
-{sample.map(item => <ItemCard key={item.id} item={item} />)}
+{products.map(item => <ItemCard key={item._id} item={item} />)}
 </div>
 </div>
 )
